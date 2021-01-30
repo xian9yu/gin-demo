20fd07c89978541037fd90760f9907236bf1144d
xian9Yu <xian9yu@gmail.com>
2021-01-30 09:19:14 +0800

    增加获取ip信息的接口

---
+++ utils/GBK2UTF8.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"bytes"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+)
+
+func GbkToUtf8(str []byte) (b []byte, err error) {
+	r := transform.NewReader(bytes.NewReader(str), simplifiedchinese.GBK.NewDecoder())
+	b, err = ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+	return
+}
+
+// transform UTF-8 bytes to GBK bytes
+func Utf8ToGbk(str []byte) (b []byte, err error) {
+	r := transform.NewReader(bytes.NewReader(str), simplifiedchinese.GBK.NewEncoder())
+	b, err = ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+	return
+}
---
+++ utils/getInfoByIp.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type IpInfo struct {
+	Ip       string `json:"ip"`       // ip
+	Pro      string `json:"pro"`      // 省会
+	ProCode  string `json:"proCode"`  // 邮政编码
+	City     string `json:"city"`     // 城市
+	CityCode string `json:"cityCode"` // 城市邮政编码
+	Addr     string `json:"addr"`     // 运营商
+}
+
+// 返回：IP地址的信息（格式：字符串的json）
+func GetCityByIp(ipAddr string) string {
+	if ipAddr == "[::1]" || ipAddr == "127.0.0.1" {
+		return "内网IP"
+	}
+	ip := IpInfo{}
+	client := http.Client{Timeout: 5 * time.Second}
+	r, _ := client.Get(`http://whois.pconline.com.cn/ipJson.jsp?json=true&ip=` + ipAddr)
+
+	body, _ := ioutil.ReadAll(r.Body)
+	res, _ := GbkToUtf8(body) // 转换编码格式 (return:[]bytes,error)
+	_ = json.Unmarshal(res, &ip)
+	if ip.City == "" {
+		ip.City = ip.Addr
+		return ip.City
+	}
+	return ip.City
+}
+
+// 返回：IP地址的信息(结构体)
+func IPInfo(ipAddr string) (ip IpInfo) {
+	if ipAddr == "[::1]" || ipAddr == "127.0.0.1" {
+		ip.City = "内网IP"
+		return ip
+	}
+	client := http.Client{Timeout: 5 * time.Second}
+	r, _ := client.Get(`http://whois.pconline.com.cn/ipJson.jsp?json=true&ip=` + ipAddr)
+
+	body, _ := ioutil.ReadAll(r.Body)
+	res, _ := GbkToUtf8(body) // 转换编码格式 (return:[]bytes,error)
+	if err := json.Unmarshal(res, &ip); err != nil {
+		return ip
+	}
+	if ip.City == "" {
+		ip.City = ip.Addr
+		return ip
+	}
+	return ip
+}
